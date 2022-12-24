f1c3a6af199dcf695313a8f7e1c99124ec7b40d9
Vamshiii2 <vvce21cse0007@vvce.ac.in>
2022-12-24 13:54:43 +0530

    Kotlinprogram

---
+++ src/main/kotlin/when.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val greetings= readln()
+    when(greetings)
+    {
+        "USA"->println("hello")
+        "India"->println("Namasthe")
+        "Russia"->println("Private")
+        else->println("I dono about it")
+    }
+}
